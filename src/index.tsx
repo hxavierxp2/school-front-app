17e63f37cdb699cc7058543a6461dc908b79a04c
LeHenriqueAlmeida <henriquexavier@lelabs.com.br>
2024-09-07 00:03:37 -0300

    update route

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import './input.css';
 import StudentsListing from './pages/listing/StudentsListing';
